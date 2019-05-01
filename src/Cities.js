29bbfd71135872f422721ede449f88072679b33c
GabbaS <margbsousa@gmail.com>
2019-05-01 12:26:03 +0100

    myweather, mylocation, mycities components created

---
+++ src/Cities.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Cities.css";
+
+export default class Cities extends Component {
+  render() {
+    return (
+      <div>myCities</div>
+    )
+  }
+}
---
+++ src/Location.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Location.css";
+
+export default class Location extends Component {
+  render() {
+    return (
+      <div>myLocation</div>
+    )
+  }
+}
