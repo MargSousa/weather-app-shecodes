bfd8a3fa8032030996597b6af2732b87ed38fba1
GabbaS <margbsousa@gmail.com>
2019-05-09 21:13:12 +0100

    weekly forecast adjustments

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ export default class App extends Component {
       <div className="app">
         <Header />
         <Location />
-        <br />
-        <Cities />
       </div>
     );
   }
--- src/WeeklyForecast.js
+++ src/WeeklyForecast.js
@@ -7,7 +7,7 @@ export default class WeeklyForecast extends Component {
   render() {
     return (
       <div className="weekly-box">
-        <table className="table">
+        <table className="week-table table-sm w-auto">
           <tbody>
             <tr>
               <td colspan="3" className="weekly-title">Weekly Forecast</td>
@@ -15,32 +15,30 @@ export default class WeeklyForecast extends Component {
             <tr>
               <td>SAT</td>
               <td><img className="weekly-icon" src={SunnyIcon} alt="Weather Icon"/></td>
-              <td>22° / 13°C</td>
+              <td>22°<span className="min-temp"> / 13°C</span></td>
             </tr>
             <tr>
               <td>SUN</td>
               <td><img className="weekly-icon" src={CloudyIcon} alt="Weather Icon"/></td>
-              <td>22° / 13°C</td>
+              <td>22°<span className="min-temp"> / 13°C</span></td>
             </tr>
             <tr>
               <td>MON</td>
               <td><img className="weekly-icon" src={SunnyIcon} alt="Weather Icon"/></td>
-              <td>22° / 13°C</td>
+              <td>22°<span className="min-temp"> / 13°C</span></td>
             </tr>
             <tr>
               <td>TUE</td>
               <td><img className="weekly-icon" src={CloudyIcon} alt="Weather Icon"/></td>
-              <td>22° / 13°C</td>
+              <td>22°<span className="min-temp"> / 13°C</span></td>
             </tr>
             <tr>
               <td>WED</td>
               <td><img className="weekly-icon" src={SunnyIcon} alt="Weather Icon"/></td>
-              <td>22° / 13°C</td>
+              <td>22°<span className="min-temp"> / 13°C</span></td>
             </tr>
-            <tr>
-              <td>THU</td>
-              <td><img className="weekly-icon" src={CloudyIcon} alt="Weather Icon"/></td>
-              <td>22° / 13°C</td>
+            <tr className="week-footer">
+              <td colspan="3"><a href="https://www.weatherbug.com/weather-forecast/now/lisboa-lisboa-po?center=51.89,-8.49,7" target="_blank">Click here for more information</a></td>
             </tr>
           </tbody>
         </table>
