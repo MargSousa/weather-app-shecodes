42048095c034a531e0740895520b930d95a0cced
GabbaS <margbsousa@gmail.com>
2019-05-12 17:54:45 +0100

    temperature button added

--- src/TemperatureButton.js
+++ src/TemperatureButton.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 
 export default class TemperatureButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  active = () => {
+    
+  }
+
   render() {
     return (
-      <div></div>  
+      <div className="temperature-button">
+        <div className="btn-group" role="group" aria-label="Basic example">
+          <button type="button" className="btn btn-secondary btn-sm">°C</button>
+          <button type="button" className="btn btn-secondary btn-sm">°F</button>
+        </div>
+      </div>  
     )
   }
 }
