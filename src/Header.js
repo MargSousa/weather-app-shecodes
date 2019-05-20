a6e114f4fa1a1781539868f4b01fdeb917ba7137
GabbaS <margbsousa@gmail.com>
2019-05-20 20:35:42 +0100

    country codes added to location

--- src/Header.js
+++ src/Header.js
@@ -33,7 +33,7 @@ export default class Header extends Component {
       let path = `weather?lat=${getlatitude}&lon=${getlongitude}&appid=${key}&units=metric`;
       
       console.log(coords);      
-      console.log(`${url}/${path}`)
+      console.log(`${url}/${path}`);
 
       let buttonTemp = document.getElementById("button-one");
       let sunriseInfo = document.getElementById('sunrise');
@@ -42,12 +42,268 @@ export default class Header extends Component {
       let tempInfo = document.getElementById('location-temperature');
       let descriptionInfo = document.getElementById('location-description');
       let cityCountryInfo = document.getElementById('location');
-
+      
       Axios.get(`${url}/${path}`).then(function(response) {
+          let isoCountries = {
+            'AF' : 'Afghanistan',
+            'AX' : 'Aland Islands',
+            'AL' : 'Albania',
+            'DZ' : 'Algeria',
+            'AS' : 'American Samoa',
+            'AD' : 'Andorra',
+            'AO' : 'Angola',
+            'AI' : 'Anguilla',
+            'AQ' : 'Antarctica',
+            'AG' : 'Antigua And Barbuda',
+            'AR' : 'Argentina',
+            'AM' : 'Armenia',
+            'AW' : 'Aruba',
+            'AU' : 'Australia',
+            'AT' : 'Austria',
+            'AZ' : 'Azerbaijan',
+            'BS' : 'Bahamas',
+            'BH' : 'Bahrain',
+            'BD' : 'Bangladesh',
+            'BB' : 'Barbados',
+            'BY' : 'Belarus',
+            'BE' : 'Belgium',
+            'BZ' : 'Belize',
+            'BJ' : 'Benin',
+            'BM' : 'Bermuda',
+            'BT' : 'Bhutan',
+            'BO' : 'Bolivia',
+            'BA' : 'Bosnia And Herzegovina',
+            'BW' : 'Botswana',
+            'BV' : 'Bouvet Island',
+            'BR' : 'Brazil',
+            'IO' : 'British Indian Ocean Territory',
+            'BN' : 'Brunei Darussalam',
+            'BG' : 'Bulgaria',
+            'BF' : 'Burkina Faso',
+            'BI' : 'Burundi',
+            'KH' : 'Cambodia',
+            'CM' : 'Cameroon',
+            'CA' : 'Canada',
+            'CV' : 'Cape Verde',
+            'KY' : 'Cayman Islands',
+            'CF' : 'Central African Republic',
+            'TD' : 'Chad',
+            'CL' : 'Chile',
+            'CN' : 'China',
+            'CX' : 'Christmas Island',
+            'CC' : 'Cocos (Keeling) Islands',
+            'CO' : 'Colombia',
+            'KM' : 'Comoros',
+            'CG' : 'Congo',
+            'CD' : 'Congo, Democratic Republic',
+            'CK' : 'Cook Islands',
+            'CR' : 'Costa Rica',
+            'CI' : 'Cote D\'Ivoire',
+            'HR' : 'Croatia',
+            'CU' : 'Cuba',
+            'CY' : 'Cyprus',
+            'CZ' : 'Czech Republic',
+            'DK' : 'Denmark',
+            'DJ' : 'Djibouti',
+            'DM' : 'Dominica',
+            'DO' : 'Dominican Republic',
+            'EC' : 'Ecuador',
+            'EG' : 'Egypt',
+            'SV' : 'El Salvador',
+            'GQ' : 'Equatorial Guinea',
+            'ER' : 'Eritrea',
+            'EE' : 'Estonia',
+            'ET' : 'Ethiopia',
+            'FK' : 'Falkland Islands (Malvinas)',
+            'FO' : 'Faroe Islands',
+            'FJ' : 'Fiji',
+            'FI' : 'Finland',
+            'FR' : 'France',
+            'GF' : 'French Guiana',
+            'PF' : 'French Polynesia',
+            'TF' : 'French Southern Territories',
+            'GA' : 'Gabon',
+            'GM' : 'Gambia',
+            'GE' : 'Georgia',
+            'DE' : 'Germany',
+            'GH' : 'Ghana',
+            'GI' : 'Gibraltar',
+            'GR' : 'Greece',
+            'GL' : 'Greenland',
+            'GD' : 'Grenada',
+            'GP' : 'Guadeloupe',
+            'GU' : 'Guam',
+            'GT' : 'Guatemala',
+            'GG' : 'Guernsey',
+            'GN' : 'Guinea',
+            'GW' : 'Guinea-Bissau',
+            'GY' : 'Guyana',
+            'HT' : 'Haiti',
+            'HM' : 'Heard Island & Mcdonald Islands',
+            'VA' : 'Holy See (Vatican City State)',
+            'HN' : 'Honduras',
+            'HK' : 'Hong Kong',
+            'HU' : 'Hungary',
+            'IS' : 'Iceland',
+            'IN' : 'India',
+            'ID' : 'Indonesia',
+            'IR' : 'Iran, Islamic Republic Of',
+            'IQ' : 'Iraq',
+            'IE' : 'Ireland',
+            'IM' : 'Isle Of Man',
+            'IL' : 'Israel',
+            'IT' : 'Italy',
+            'JM' : 'Jamaica',
+            'JP' : 'Japan',
+            'JE' : 'Jersey',
+            'JO' : 'Jordan',
+            'KZ' : 'Kazakhstan',
+            'KE' : 'Kenya',
+            'KI' : 'Kiribati',
+            'KR' : 'Korea',
+            'KW' : 'Kuwait',
+            'KG' : 'Kyrgyzstan',
+            'LA' : 'Lao People\'s Democratic Republic',
+            'LV' : 'Latvia',
+            'LB' : 'Lebanon',
+            'LS' : 'Lesotho',
+            'LR' : 'Liberia',
+            'LY' : 'Libyan Arab Jamahiriya',
+            'LI' : 'Liechtenstein',
+            'LT' : 'Lithuania',
+            'LU' : 'Luxembourg',
+            'MO' : 'Macao',
+            'MK' : 'Macedonia',
+            'MG' : 'Madagascar',
+            'MW' : 'Malawi',
+            'MY' : 'Malaysia',
+            'MV' : 'Maldives',
+            'ML' : 'Mali',
+            'MT' : 'Malta',
+            'MH' : 'Marshall Islands',
+            'MQ' : 'Martinique',
+            'MR' : 'Mauritania',
+            'MU' : 'Mauritius',
+            'YT' : 'Mayotte',
+            'MX' : 'Mexico',
+            'FM' : 'Micronesia, Federated States Of',
+            'MD' : 'Moldova',
+            'MC' : 'Monaco',
+            'MN' : 'Mongolia',
+            'ME' : 'Montenegro',
+            'MS' : 'Montserrat',
+            'MA' : 'Morocco',
+            'MZ' : 'Mozambique',
+            'MM' : 'Myanmar',
+            'NA' : 'Namibia',
+            'NR' : 'Nauru',
+            'NP' : 'Nepal',
+            'NL' : 'Netherlands',
+            'AN' : 'Netherlands Antilles',
+            'NC' : 'New Caledonia',
+            'NZ' : 'New Zealand',
+            'NI' : 'Nicaragua',
+            'NE' : 'Niger',
+            'NG' : 'Nigeria',
+            'NU' : 'Niue',
+            'NF' : 'Norfolk Island',
+            'MP' : 'Northern Mariana Islands',
+            'NO' : 'Norway',
+            'OM' : 'Oman',
+            'PK' : 'Pakistan',
+            'PW' : 'Palau',
+            'PS' : 'Palestinian Territory, Occupied',
+            'PA' : 'Panama',
+            'PG' : 'Papua New Guinea',
+            'PY' : 'Paraguay',
+            'PE' : 'Peru',
+            'PH' : 'Philippines',
+            'PN' : 'Pitcairn',
+            'PL' : 'Poland',
+            'PT' : 'Portugal',
+            'PR' : 'Puerto Rico',
+            'QA' : 'Qatar',
+            'RE' : 'Reunion',
+            'RO' : 'Romania',
+            'RU' : 'Russian Federation',
+            'RW' : 'Rwanda',
+            'BL' : 'Saint Barthelemy',
+            'SH' : 'Saint Helena',
+            'KN' : 'Saint Kitts And Nevis',
+            'LC' : 'Saint Lucia',
+            'MF' : 'Saint Martin',
+            'PM' : 'Saint Pierre And Miquelon',
+            'VC' : 'Saint Vincent And Grenadines',
+            'WS' : 'Samoa',
+            'SM' : 'San Marino',
+            'ST' : 'Sao Tome And Principe',
+            'SA' : 'Saudi Arabia',
+            'SN' : 'Senegal',
+            'RS' : 'Serbia',
+            'SC' : 'Seychelles',
+            'SL' : 'Sierra Leone',
+            'SG' : 'Singapore',
+            'SK' : 'Slovakia',
+            'SI' : 'Slovenia',
+            'SB' : 'Solomon Islands',
+            'SO' : 'Somalia',
+            'ZA' : 'South Africa',
+            'GS' : 'South Georgia And Sandwich Isl.',
+            'ES' : 'Spain',
+            'LK' : 'Sri Lanka',
+            'SD' : 'Sudan',
+            'SR' : 'Suriname',
+            'SJ' : 'Svalbard And Jan Mayen',
+            'SZ' : 'Swaziland',
+            'SE' : 'Sweden',
+            'CH' : 'Switzerland',
+            'SY' : 'Syrian Arab Republic',
+            'TW' : 'Taiwan',
+            'TJ' : 'Tajikistan',
+            'TZ' : 'Tanzania',
+            'TH' : 'Thailand',
+            'TL' : 'Timor-Leste',
+            'TG' : 'Togo',
+            'TK' : 'Tokelau',
+            'TO' : 'Tonga',
+            'TT' : 'Trinidad And Tobago',
+            'TN' : 'Tunisia',
+            'TR' : 'Turkey',
+            'TM' : 'Turkmenistan',
+            'TC' : 'Turks And Caicos Islands',
+            'TV' : 'Tuvalu',
+            'UG' : 'Uganda',
+            'UA' : 'Ukraine',
+            'AE' : 'United Arab Emirates',
+            'GB' : 'United Kingdom',
+            'US' : 'United States',
+            'UM' : 'United States Outlying Islands',
+            'UY' : 'Uruguay',
+            'UK' : 'United Kingdom',
+            'UZ' : 'Uzbekistan',
+            'VU' : 'Vanuatu',
+            'VE' : 'Venezuela',
+            'VN' : 'Viet Nam',
+            'VG' : 'Virgin Islands, British',
+            'VI' : 'Virgin Islands, U.S.',
+            'WF' : 'Wallis And Futuna',
+            'EH' : 'Western Sahara',
+            'YE' : 'Yemen',
+            'ZM' : 'Zambia',
+            'ZW' : 'Zimbabwe'
+        };
+        
+        let countryCode = response.data.sys.country;
+        let country = response.data.sys.country;
+        
+        if (isoCountries.hasOwnProperty(countryCode)) {
+          country = isoCountries[countryCode];
+        }          
+        console.log(country);
+
+        let city = response.data.name;
         let description = (response.data.weather[0].description);
         let icon = response.data.weather[0].icon;
-        let city = response.data.name;
-        let country = response.data.sys.country;
         let temperature = Math.round(response.data.main.temp);
         let temperatureFahr = Math.round(temperature * (9/5) + 32);
         let wind = response.data.wind.speed;
@@ -89,29 +345,11 @@ export default class Header extends Component {
         } else {
           tempInfo.innerHTML = `${temperatureFahr}°F`;
         }
-
-
-        let isoCountries = {
-          'PT': 'Portugal',
-          'UK': 'United Kingdom'
-        }
-
-        //let countryWhat = response.data.sys.country;
-
-        //isoCountries(function(element){ 
-        //  let nowCountry = element[countryWhat];
-        //  console.log(nowCountry);
-        //});
-
-        //let country1 = isoCountries.map(function(element) { return element[countryWhat] })
-        //console.log(country1);
-
       });
     });
 
     let now = this.state.date;
 
-    console.log(now.getDay());
     let getTime = document.getElementById('location-time');
     let weekDays = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let getWeekDay = weekDays[now.getDay()];
